2255b713555796caf0247f68b02e0eda25f344a0
curt-tigges <tetrahedrex@gmail.com>
2022-11-04 23:45:41 -0400

    Added solutions for day 3

--- w0d3/utils.py
+++ w0d3/utils.py
@@ -6,6 +6,7 @@ import torchvision
 import numpy as np
 import pandas as pd
 
+
 def plot_loss_and_accuracy(loss_list, accuracy_list):
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -13,31 +14,43 @@ def plot_loss_and_accuracy(loss_list, accuracy_list):
     fig.add_trace(go.Scatter(y=loss_list, name="Loss"), secondary_y=False)
 
     n_batches = len(loss_list) // len(accuracy_list)
-    x = list(range(n_batches-1, len(loss_list), n_batches))
+    x = list(range(n_batches - 1, len(loss_list), n_batches))
     fig.add_trace(go.Scatter(y=accuracy_list, x=x, name="Accuracy"), secondary_y=True)
 
     fig.update_layout(
         title_text="CNN training loss & test accuracy",
-        template="simple_white", 
-        xaxis_range=[0, len(loss_list)], 
+        template="simple_white",
+        xaxis_range=[0, len(loss_list)],
         yaxis2_range=[0, 1],
-        yaxis2_tickformat=".0%", 
-        hovermode="x unified"
+        yaxis2_tickformat=".0%",
+        hovermode="x unified",
     )
 
     fig.show()
+
 
 def test_batchnorm2d_module(BatchNorm2d):
     """The public API of the module should be the same as the real PyTorch version."""
     num_features = 2
     bn = BatchNorm2d(num_features)
     assert bn.num_features == num_features
-    assert isinstance(bn.weight, t.nn.parameter.Parameter), f"weight has wrong type: {type(bn.weight)}"
-    assert isinstance(bn.bias, t.nn.parameter.Parameter), f"bias has wrong type: {type(bn.bias)}"
-    assert isinstance(bn.running_mean, t.Tensor), f"running_mean has wrong type: {type(bn.running_mean)}"
-    assert isinstance(bn.running_var, t.Tensor), f"running_var has wrong type: {type(bn.running_var)}"
-    assert isinstance(bn.num_batches_tracked, t.Tensor), f"num_batches_tracked has wrong type: {type(bn.num_batches_tracked)}"
+    assert isinstance(
+        bn.weight, t.nn.parameter.Parameter
+    ), f"weight has wrong type: {type(bn.weight)}"
+    assert isinstance(
+        bn.bias, t.nn.parameter.Parameter
+    ), f"bias has wrong type: {type(bn.bias)}"
+    assert isinstance(
+        bn.running_mean, t.Tensor
+    ), f"running_mean has wrong type: {type(bn.running_mean)}"
+    assert isinstance(
+        bn.running_var, t.Tensor
+    ), f"running_var has wrong type: {type(bn.running_var)}"
+    assert isinstance(
+        bn.num_batches_tracked, t.Tensor
+    ), f"num_batches_tracked has wrong type: {type(bn.num_batches_tracked)}"
     print("All tests in `test_batchnorm2d_module` passed!")
+
 
 def test_batchnorm2d_forward(BatchNorm2d):
     """For each channel, mean should be very close to 0 and std kinda close to 1 (because of eps)."""
@@ -48,8 +61,11 @@ def test_batchnorm2d_forward(BatchNorm2d):
     out = bn(x)
     assert x.shape == out.shape
     t.testing.assert_close(out.mean(dim=(0, 2, 3)), t.zeros(num_features))
-    t.testing.assert_close(out.std(dim=(0, 2, 3)), t.ones(num_features), atol=1e-3, rtol=1e-3)
+    t.testing.assert_close(
+        out.std(dim=(0, 2, 3)), t.ones(num_features), atol=1e-3, rtol=1e-3
+    )
     print("All tests in `test_batchnorm2d_forward` passed!")
+
 
 def test_batchnorm2d_running_mean(BatchNorm2d):
     """Over repeated forward calls with the same data in train mode, the running mean should converge to the actual mean."""
@@ -72,16 +88,17 @@ def test_batchnorm2d_running_mean(BatchNorm2d):
 
 
 # def compare_my_resnet_to_pytorch(myresnet):
-    
+
 #     their_state = torchvision.models.resnet34().state_dict().items()
 #     your_state = myresnet.state_dict().items()
-    
+
 #     df = pd.DataFrame.from_records(
 #         [(tk, tuple(tv.shape), mk, tuple(mv.shape)) for ((tk, tv), (mk, mv)) in zip(their_state, your_state)],
 #         columns=["their name", "their shape", "your name", "your shape"],
 #     )
 #     with pd.option_context("display.max_rows", None):  # type: ignore
 #         display(df)
+
 
 def print_param_count(*models, display_df=True, use_state_dict=True):
     """
@@ -98,15 +115,35 @@ def print_param_count(*models, display_df=True, use_state_dict=True):
     df_list = []
     gmap_list = []
     for i, model in enumerate(models, start=1):
-        print(f"Model {i}, total params = {sum([param.numel() for name, param in model.named_parameters()])}")
-        iterator = model.state_dict().items() if use_state_dict else model.named_parameters()
-        df = pd.DataFrame([
-            {f"name_{i}": name, f"shape_{i}": tuple(param.shape), f"num_params_{i}": param.numel()}
-            for name, param in iterator
-        ]) if (i == 1) else pd.DataFrame([
-            {f"num_params_{i}": param.numel(), f"shape_{i}": tuple(param.shape), f"name_{i}": name}
-            for name, param in iterator
-        ])
+        print(
+            f"Model {i}, total params = {sum([param.numel() for name, param in model.named_parameters()])}"
+        )
+        iterator = (
+            model.state_dict().items() if use_state_dict else model.named_parameters()
+        )
+        df = (
+            pd.DataFrame(
+                [
+                    {
+                        f"name_{i}": name,
+                        f"shape_{i}": tuple(param.shape),
+                        f"num_params_{i}": param.numel(),
+                    }
+                    for name, param in iterator
+                ]
+            )
+            if (i == 1)
+            else pd.DataFrame(
+                [
+                    {
+                        f"num_params_{i}": param.numel(),
+                        f"shape_{i}": tuple(param.shape),
+                        f"name_{i}": name,
+                    }
+                    for name, param in iterator
+                ]
+            )
+        )
         display(df)
         df_list.append(df)
         gmap_list.append(np.log(df[f"num_params_{i}"]))
@@ -114,15 +151,21 @@ def print_param_count(*models, display_df=True, use_state_dict=True):
     for i in range(1, len(models) + 1):
         df[f"num_params_{i}"] = df[f"num_params_{i}"].astype(int)
     if len(models) > 1:
-        param_counts = [df[f"num_params_{i}"].values.tolist() for i in range(1, len(models) + 1)]
-        if all([param_counts[0] == param_counts[i] for i in range(1, len(param_counts))]):
+        param_counts = [
+            df[f"num_params_{i}"].values.tolist() for i in range(1, len(models) + 1)
+        ]
+        if all(
+            [param_counts[0] == param_counts[i] for i in range(1, len(param_counts))]
+        ):
             print("All parameter counts match!")
         else:
             print("Parameter counts don't match up exactly.")
     if display_df:
         s = df.style
         for i in range(1, len(models) + 1):
-            s = s.background_gradient(cmap="viridis", subset=[f"num_params_{i}"], gmap=gmap_list[i-1])
+            s = s.background_gradient(
+                cmap="viridis", subset=[f"num_params_{i}"], gmap=gmap_list[i - 1]
+            )
         with pd.option_context("display.max_rows", 1000):
             display(s)
     else:
